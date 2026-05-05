a184d9c2d1dd9209dd540b442edc8473e6b2cac2
lawalalx <lawal.alx@gmail.com>
2026-05-05 08:41:18 +0100

    removed reamd

--- src/index.ts
+++ src/index.ts
@@ -1036,8 +1036,9 @@ async function startServer() {
     const server = new MastraServer({ app: app as any, mastra });
     await server.init();
 
-    const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+    // const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
+    const PORT = 10000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
